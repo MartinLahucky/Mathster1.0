4df09357f3d61b837da143880399370b2ef04466
Martin <lahuckmartin@gmail.com>
2020-06-17 22:38:20 +0200

    Fixnutí pro menší čísla

--- Mathster/Mathster/Souhrn.xaml.cs
+++ Mathster/Mathster/Souhrn.xaml.cs
@@ -31,7 +31,7 @@ namespace Mathster
             foreach (var priklad in fronta)
             {
                 priklady.Add(new Vysledek(priklad.VratPriklad()));
-                if (priklad.UzivateluvVstup >= 1000)
+                if (priklad.PrvniCislo >= 1000 || priklad.PrvniCislo >= 1000 && priklad.UzivateluvVstup >= 1000)
                 {
                     VysledkyList.RowHeight = 80;
                 }
