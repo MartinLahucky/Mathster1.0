d6387bc6893dfb085ad498554821e1a8fad15343
Martin <lahuckmartin@gmail.com>
2020-06-25 16:56:16 +0200

    Formátování Cellů v ListView snad už dobře

--- Mathster/Mathster/Priklad.cs
+++ Mathster/Mathster/Priklad.cs
@@ -82,7 +82,7 @@ namespace Mathster
         }
         public string VratPriklad()
         {
-            if (prvniCislo >= 1000)
+            if (PrvniCislo >= 100000 || DruheCislo >= 100000 || UzivateluvVstup >= 100000)
             {
                 switch (DruhPrikladu)
                 {
