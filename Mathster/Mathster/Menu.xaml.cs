9115428d2d5eb6676b5ec8893c9341ff2ec6205c
Martin <lahuckmartin@gmail.com>
2020-07-19 23:23:32 +0200

    Optimalizace importů

--- Mathster/Mathster/Menu.xaml.cs
+++ Mathster/Mathster/Menu.xaml.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Mathster.Helpers.Resources;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
