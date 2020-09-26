771a0a9c6ebdb43815d9f7724e4bd79b6ea03965
Martin Lahučký <lahuckmartin@gmail.com>
2020-09-26 11:56:15 +0200

    Tato změna přináší větší revoluci než naše Sametová Revoluce

---
+++ Mathster/Mathster/Helpers/Custom_UI/CS_Entry.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Mathster.Helpers.Custom_UI
+{
+    public class CS_Entry : Entry
+    {
+        public CS_Entry()
+        {
+            BackgroundColor = Color.Transparent;
+        }
+    }
+}
