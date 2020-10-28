7441795935c8bfbbf6a8e85606654104fa7e0ebd
Martin Lahučký <lahuckmartin@gmail.com>
2020-10-28 12:12:53 +0100

    Double trouble

--- Mathster/Mathster.Android/MainActivity.cs
+++ Mathster/Mathster.Android/MainActivity.cs
@@ -1,7 +1,9 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Widget;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 using Environment = System.Environment;
@@ -24,6 +26,25 @@ namespace Mathster.Android
             // Database Declaration
             string fullPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "mathster_db.sqlite");
             LoadApplication(new App(fullPath));
+        }
+        //Double press pro exit 
+        private long lastPress;
+        public override void OnBackPressed()
+        {
+            // source https://stackoverflow.com/a/27124904/3814729
+            long currentTime = DateTime.UtcNow.Ticks / TimeSpan.TicksPerMillisecond;
+
+            // source https://stackoverflow.com/a/14006485/3814729
+            if (currentTime - lastPress > 5000)
+            {
+                //TODO Vymyslet jiný text
+                Toast.MakeText(this, "<-- + 1 = X", ToastLength.Long).Show();
+                lastPress = currentTime;
+            }
+            else
+            {
+                base.OnBackPressed();
+            }
         }
     }
 }
