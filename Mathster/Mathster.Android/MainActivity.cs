6efbe3a3abdf681d83136b65212180c4c88b078e
Martin Lahučký <lahuckmartin@gmail.com>
2020-10-30 11:59:38 +0100

    Připravení pro následují vydání

--- Mathster/Mathster.Android/MainActivity.cs
+++ Mathster/Mathster.Android/MainActivity.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
-using Android.Widget;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 using Environment = System.Environment;
