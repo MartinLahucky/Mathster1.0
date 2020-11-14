3e617e8add8e9c383f4f4d3e71a5784d20551b59
Martin <lahuckmartin@gmail.com>
2020-11-14 18:25:37 +0100

    Verze Aplikace updated

--- Mathster/Mathster/Nastaveni.xaml.cs
+++ Mathster/Mathster/Nastaveni.xaml.cs
@@ -34,7 +34,7 @@ namespace Mathster
                 }
             });
             Task.WaitAll(task);
-            OAplikaciVerze.Text = AppResource.OAplikaciVerze + "Public Preview 1.0";
+            OAplikaciVerze.Text = AppResource.OAplikaciVerze + "Video Preview";
         }
         private async void DarkModeSwitch_Toggled(object sender, ToggledEventArgs e)
         {
